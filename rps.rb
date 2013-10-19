3f34d4f994bf5417bc121d5bad2ea1687418c1ff
Angela <angelahdezd@gmail.com>
2013-10-19 12:56:35 +0100

    las cookies funcionan en www.example.com

--- rps.rb
+++ rps.rb
@@ -8,8 +8,15 @@ builder = Rack:: Builder.new do
   use Rack::Session::Cookie, 
       :key => 'rack.session', 
       :domain => 'example.com',
-      :secret => 'some_secret'
+      :secret => 'some_secret',
+      :expire_after => 30
   run RockPaperScissors::App.new
+
 end
 
-Rack::Handler::Thin.run builder
+#Rack::Handler::Thin.run builder
+use Rack::Server.start(
+  	:app=> builder,
+  	:Port=>9393,
+  	:server=>'thin',
+  	:host => 'www.example.com')
